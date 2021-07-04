51b840c01e746ab6841f04061e9a9a78ad3eedb1
Adie Williams <adie@hello.codes>
2021-07-04 12:23:39 +0100

    Add cypress env var to reduce repitition

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -1,6 +1,6 @@
 describe('Home page', () => {
   it('Renders event', () => {
-    cy.visit('http://localhost:3000');
+    cy.visit(Cypress.env('baseUrl'));
 
     cy.get('.event-title').contains('Ukraine V England');
     cy.get('.event-time').contains('July 3');
